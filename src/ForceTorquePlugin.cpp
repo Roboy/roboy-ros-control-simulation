799cb8b29aa46877ab3bf3a5ae51f1aae415d7a3
Raphael Dürscheid <rd@generalinterfaces.com>
2016-08-31 10:40:22 +0200

    Fix Gazebo 5 member names

--- src/ForceTorquePlugin.cpp
+++ src/ForceTorquePlugin.cpp
@@ -54,17 +54,17 @@ void ForceTorquePlugin::OnUpdate() {
     roboy_simulation::ForceTorque msg;
     msg.leg = leg;
 #if GAZEBO_MAJOR_VERSION < 7
-    math::Vector3 force = parentSensor->Force();
-    math::Vector3 torque = parentSensor->Torque();
-    msg.force.x = force.X();
-    msg.force.y = force.Y();
-    msg.force.z = force.Z();
-    msg.torque.x = torque.X();
-    msg.torque.y = torque.Y();
-    msg.torque.z = torque.Z();
+    math::Vector3 force = parentSensor->GetForce();
+    math::Vector3 torque = parentSensor->GetTorque();
+    msg.force.x = force.x;
+    msg.force.y = force.y;
+    msg.force.z = force.z;
+    msg.torque.x = torque.x;
+    msg.torque.y = torque.y;
+    msg.torque.z = torque.z;
 #else
-    ignition::math::Vector3d force = parentSensor->Force();
-    ignition::math::Vector3d torque = parentSensor->Torque();
+    ignition::math::Vector3d force = parentSensor->GetForce();
+    ignition::math::Vector3d torque = parentSensor->GetTorque();
     msg.force.x = force.X();
     msg.force.y = force.Y();
     msg.force.z = force.Z();
